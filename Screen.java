bd848712a34070e76c084cfadeea5301ab6c2085
Drew Honer <drewh3781@gmail.com>
2024-03-12 21:41:39 -0700

    modified:   Screen.java 	modified:   Square.java

--- Screen.java
+++ Screen.java
@@ -1,35 +1,38 @@
 import javax.swing.JPanel;
 import javax.swing.JButton;
-import javax.swing.JTextPane;
-import java.awt.Font;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import javax.imageio.ImageIO;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseEvent;
 
 public class Screen extends JPanel implements MouseListener,ActionListener{
 
     private JButton clearButton;
-    private int[][] array;
+    private Square[][] array;
+    private int x,y;
+
     public Screen(){
         setLayout(null);
         setFocusable(true);
+        
 
-        array =  new  int[100][100];
+        array = new Square[100][100];
+
+        for(int row=0;row<array.length;row++){
+            for(int col=0;col<array[row].length;col++){
+                array[row][col] = new Square(Color.white);
+            }
+        }
 
         clearButton = new JButton("Clear");
         clearButton.addActionListener(this);
-        clearButton.setBounds(700,100,50,20);
+        clearButton.setBounds(640,100,100,40);
         add(clearButton);
+
+        addMouseListener(this);
     }
 
 
@@ -51,15 +54,50 @@ public class Screen extends JPanel implements MouseListener,ActionListener{
             g.drawLine(600, 250 + i*50, 750, 250 + i*50);
             g.drawLine(600 + i*50, 250, 600 + i*50, 400);
         }
+
+        for(int row=0;row<array.length;row++){
+            for(int col=0;col<array[row].length;col++){
+                array[row][col].draw(g);
+            }
+        }
+
+        g.setColor(Color.red);
+        g.fillRect(601,251,49,49);
+
+        g.setColor(Color.orange);
+        g.fillRect(651,251,49,49);
+
+        g.setColor(Color.yellow);
+        g.fillRect(701,251,49,49);
+
+        g.setColor(Color.green);
+        g.fillRect(601,301,49,49);
+
+        g.setColor(Color.blue);
+        g.fillRect(651,301,49,49);
+
+        g.setColor(new Color(0,128,255));
+        g.fillRect(701,301,49,49);
+
+        g.setColor(new Color(255,51,153));
+        g.fillRect(601,351,49,49);
+
+        g.setColor(Color.black);;
+        g.fillRect(651,351,49,49);
+
+        g.setColor(Color.white);;
+        g.fillRect(701,351,49,49);
+        
     }
 
 
 
     public void mousePressed(MouseEvent e) {
-        if (e.getX() >= 50 && e.getX() <= 550 && e.getY() >= 50 && e.getY() <= 550) {
-            
-        }
         
+        if(e.getX() >= 50 && e.getX() <= 550 && e.getY()>= 50 && e.getY() <= 550){
+        
+        }
+
         repaint();
     }
     
@@ -72,6 +110,10 @@ public class Screen extends JPanel implements MouseListener,ActionListener{
 
     public void mouseClicked(MouseEvent e) {}
 
+
+    public void clear(){
+        
+    }
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == clearButton) {
--- Square.java
+++ Square.java
@@ -1,18 +1,22 @@
 import java.awt.Color;
+import java.awt.Graphics;
+
 
 public class Square{
-
+    int x,y;
     Color color;
 
-    public Square(){
+    public Square(Color color){
+        this.color = color;
 
     }
 
-    public void draw(){
-
+    public void draw(Graphics g){
+        g.setColor(color);
+        g.fillRect(x, y, 4, 4);
     }
 
-    public void setColor(){
-
+    public void setColor(Color color){
+        this.color = color;
     }
 }
